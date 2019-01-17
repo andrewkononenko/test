20907e00e93ca26787d617e20aa705496a9d7e5e
Andrey Kononenko <andrey.kononenko@dataart.com>
2019-01-17 14:01:28 +0200

    Added Health

--- src/main/java/com/example/aws/test/TestApplication.java
+++ src/main/java/com/example/aws/test/TestApplication.java
@@ -2,9 +2,22 @@ package com.example.aws.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestApplication {
+
+	@GetMapping("/init")
+	public String getInit() {
+		return "Init endpoint";
+	}
+
+    @GetMapping("/")
+    public String getHealth() {
+        return "Status up";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
